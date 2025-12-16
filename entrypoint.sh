29e0b4926107676d3cc04b400b29b60e09ec812e
sushanth <sai.sandhu2005@gmail.com>
2025-12-16 12:06:54 +0530

    changed celery to run within the same container

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ python manage.py migrate
 if [ "$#" -gt 0 ]; then
     exec "$@"
 else
+    # Start Celery worker in background
+    celery -A smartly worker -l info &
+    
+    # Start Gunicorn
     exec gunicorn --bind 0.0.0.0:${PORT:-8000} smartly.wsgi:application
 fi
+
 
