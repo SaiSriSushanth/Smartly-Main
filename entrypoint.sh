e3ecd5963eaa999120cd3bbe4405a2f6bf99509c
sushanth <sai.sandhu2005@gmail.com>
2025-12-16 12:39:48 +0530

    added manual startup command for celery

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,6 @@ else
     if [ -z "$CELERY_BROKER_URL" ]; then
         echo "WARNING: CELERY_BROKER_URL is not set. Celery might fail."
     else
-        echo "CELERY_BROKER_URL is set."
-    fi
-    
-    echo "Starting Celery worker..."
-    celery -A smartly worker -l info &
-    
-    # Wait a moment to ensure Celery starts (and log any immediate errors)
-    sleep 5
-    
-    # Start Gunicorn
-    echo "Starting Gunicorn..."
+    echo "Starting default Gunicorn..."
     exec gunicorn --bind 0.0.0.0:${PORT:-8000} smartly.wsgi:application
-
 fi
